ef3a4bf939979446a3245e89a9b459a3caa12e72
AllenHirmiz <allenaliraqi@gmail.com>
2023-10-16 23:34:45 +1100

    Update mongodb name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Book-Search-Engine"
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/BookSearchEngine"
 );
 
 module.exports = mongoose.connection;
